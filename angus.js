1fd87b2d72a401ae13f93c5964cb80cec6dd978c
anglee <ang.lee@gmail.com>
2014-10-21 07:31:26 -0400

    Add promise utils using  constructor

--- angus.js
+++ angus.js
@@ -9,6 +9,15 @@
         agKnife.delay(1000)
             .then(function() { return "ABC"; })
             .then(agDebug.log);
+
+        agKnife.all([agKnife.when("aaa"), agKnife.when("bbb")])
+            .then(agDebug.log);
+
+        var delayedP = agKnife.delay(500).then(function() {
+          return "AAA"
+        });
+        agKnife.race([delayedP, agKnife.resolve("BBB")])
+            .then(agDebug.log);
       }
     };
   });
@@ -16,31 +25,79 @@
   angus.factory("agKnife", function($q, $timeout) {
     return {
       delay: function(ms) {
-        var deferred = $q.defer();
-        $timeout(function() {
-          deferred.resolve();
-        }, ms);
-        return deferred.promise;
+        return new $q(function(resolve) {
+          $timeout(function() {
+            resolve();
+          }, ms);
+        });
       },
       when: function(foo) {
-        return $q.when(foo);
+        return new $q(function(resolve) {
+          $timeout(function() {
+            resolve(foo);
+          });
+        });
+      },
+      resolve: function(foo) {
+        return new $q(function(resolve) {
+          resolve(foo);
+        });
+      },
+      reject: function(foo) {
+        return new $q(function(resolve) {
+          reject(foo);
+        });
+      },
+      all: function(promises) {
+        return new $q(function(resolve, reject) {
+          var results = [];
+          var fulfilledCount = 0;
+          promises.forEach(function(p, index) {
+            p.then(function(result) {
+              results[index] = result;
+              fulfilledCount++;
+              if (fulfilledCount === promises.length) {
+                resolve(results);
+              }
+            }, function(error) {
+              reject(error);
+            });
+          });
+        });
+      },
+      race: function(promises) {
+        return new $q(function(resolve, reject) {
+          promises.forEach(function(p, index) {
+            p.then(function(result) {
+              resolve(result);
+            }, function(error) {
+              reject(error);
+            });
+          });
+        });
       }
-    }
+    };
   });
 
-  angus.factory("agDebug", function(agKnife) {
+  angus.factory("agDebug", function(
+      $injector, $q, $timeout, $http, agKnife) {
     return {
+      $injector: $injector,
+      $q: $q,
+      $timeout: $timeout,
+      $http: $http,
       agKnife: agKnife,
       log: function(data) {
         console.log("[Debug]", data);
         this.$0 = data;
       }
-    }
+    };
   });
 
   angus.run(function(agDebug) {
     window.agDebug = agDebug;
-    window.aaa = agDebug;
+    window.agd = agDebug;
+    window.agKnife = agDebug.agKnife;
   });
 
 })(angular);
