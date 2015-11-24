fab80d37409284547b955521ff4c06545e3514fd
anglee <ang.lee@gmail.com>
2015-11-24 05:40:52 -0500

    Add index.less

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,29 @@ var path = require('path');
 
 module.exports = {
 	devtool: 'eval-source-map',
-	entry: './app/index.js',
+	entry: ['./app/index.js', './app/index.less'],
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'bundle.js',
 		publicPath: '/static/'
 	},
 	module: {
-		loaders: [{
-			test: /\.js$/,
-			loaders: ['babel-loader'],
-			include: path.join(__dirname, 'app')
-		}]
+		loaders: [
+			{
+				test: /\.js$/,
+				loaders: ['babel-loader'],
+				include: path.join(__dirname, 'app')
+			},
+			{
+				test: /\.css$/,
+				loader: 'style!css',
+				include: path.join(__dirname, 'app')
+			},
+			{
+				test: /\.less$/,
+				loader: 'style!css!less',
+				include: path.join(__dirname, 'app')
+			}
+		]
 	}
 };
